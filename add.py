42de17e698fde0d5723cfefa7ac1fc1aba7e8840
ocfunk <ocfunk@googlemail.com>
2024-08-19 11:35:45 +0200

    Include add and sub files

---
+++ add.py
@@ -0,0 +1,5 @@
+x = int(input("enter the first number: "))
+y = int(input("enter the second number: "))
+
+result = x + y
+print("The sum of the two numbers is: ", result)
---
+++ sub.py
@@ -0,0 +1,5 @@
+x = int(input("enter the first number: "))
+y = int(input("enter the second number: "))
+
+result = x - y
+print("The difference of the two numbers is: ", result)
