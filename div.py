e56e347f5d0a68ed65521c835e4fb794f722a005
ocfunk <ocfunk@googlemail.com>
2024-08-19 14:16:04 +0200

    New branch inlc. test file

--- div.py
+++
@@ -1,9 +0,0 @@
-x = int(input("enter the first number : "))
-y = int(input("enter the second number : "))
-
-result = x/y
-print(result)x = int(input("enter the first number : "))
-y = int(input("enter the second number : "))
-
-result = x/y
-print(result)
